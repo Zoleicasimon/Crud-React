55f88bfa7459d9db1b7f535367302092ab592328
zoleicasimon <zoleicas@hotmail.com>
2024-03-11 11:59:53 -0300

    Configuracion inicial

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+// empezar limpiando el proyecto
+
+function App() {
+ 
+  return (
+    <>
+     <div>
+      <h1>Empezamos</h1>
+     </div>
+    </>
+  )
+}
+
+export default App
